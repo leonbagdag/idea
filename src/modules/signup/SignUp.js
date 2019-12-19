5d5f8330121933dd649ab9a97b2129edc4de8f24
Omar Khaled <omarkhaledvii@gmail.com>
2019-12-19 02:12:27 +0200

    Fixed Sign Up form UI

--- src/modules/signup/SignUp.js
+++ src/modules/signup/SignUp.js
@@ -1,18 +1,13 @@
 import React, { useState, useEffect } from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import "./SignUp.css";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    "& > *": {
-      margin: theme.spacing(1),
-      width: 200
-    }
-  }
-}));
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import "./SignUp.css";
 
 const SignUp = () => {
   const [user, setUser] = useState({
@@ -30,70 +25,94 @@ const SignUp = () => {
     // call back-end wa adelo al user value
   };
   return (
-    <Grid container direction="row" justify="center" alignItems="center">
-      >
+    <Container component="main" maxWidth="xs">
+      {/* <CssBaseline /> */}
       <div className="SignUp-Form">
-        <h1>Online Shop</h1>
-        {/* <img src="https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/06/shopping-online.jpg" alt="cart" max-width="300px" max-height="300px"></img> */}
-        <h3>Sign Up</h3>
-        <Grid className="grid" item xs={6} sm={3}>
-          <TextField
-            name="firstName"
-            label="First Name"
-            variant="outlined"
-            onChange={inputChange}
-          />
-        </Grid>
+        {/* <h1>Online Shop</h1> */}
+        <img
+          src="https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/06/shopping-online.jpg"
+          alt="cart"
+          width="250"
+          max-height="50"
+        ></img>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={"form"} style={{ marginTop: 24 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="firstName"
+                label="First Name"
+                variant="outlined"
+                fullWidth
+                onChange={inputChange}
+              />
+            </Grid>
 
-        <Grid className="grid" item xs={6} sm={3}>
-          <TextField
-            name="lastName"
-            label="Last Name"
-            variant="outlined"
-            onChange={inputChange}
-          />
-        </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="lastName"
+                label="Last Name"
+                variant="outlined"
+                fullWidth
+                onChange={inputChange}
+              />
+            </Grid>
 
-        <Grid className="grid" item xs={6}>
-          <TextField
-            className="TFW"
-            name="email"
-            id="outlined-basic"
-            label="Email"
-            variant="outlined"
-            onChange={inputChange}
-          />
-        </Grid>
+            <Grid item xs={12}>
+              <TextField
+                name="email"
+                id="outlined-basic"
+                label="Email"
+                variant="outlined"
+                fullWidth
+                onChange={inputChange}
+              />
+            </Grid>
 
-        <Grid className="grid" item xs={6}>
-          <TextField
-            className="TFW"
-            name="password"
-            id="outlined-basic"
-            label="Password"
-            variant="outlined"
-            onChange={inputChange}
-          />
-        </Grid>
-        <Grid className="grid" item xs={6}>
-          <TextField
-            className="TFW"
-            name="phoneNum"
-            id="outlined-basic"
-            label="Phone Number"
-            variant="outlined"
-            onChange={inputChange}
-          />
-        </Grid>
-        <Grid className="grid" item xs={6}>
-          <Button variant="contained" color="primary" onClick={signUp}>
-            Sign Up
-          </Button>
-        </Grid>
-
-        <a href="">Already a Customer? Sign In.</a>
+            <Grid item xs={12}>
+              <TextField
+                name="password"
+                id="outlined-basic"
+                label="Password"
+                variant="outlined"
+                fullWidth
+                onChange={inputChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                name="phoneNum"
+                id="outlined-basic"
+                label="Phone Number"
+                variant="outlined"
+                fullWidth
+                onChange={inputChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                className="submitBTN"
+                variant="contained"
+                color="primary"
+                fullWidth
+                onClick={signUp}
+              >
+                Sign Up
+              </Button>
+            </Grid>
+            <Grid container justify="flex-end">
+              <Grid item>
+                <Link href="" variant="body2">
+                  Already a Customer? Sign In.
+                </Link>
+              </Grid>
+            </Grid>
+          </Grid>
+        </form>
       </div>
-    </Grid>
+    </Container>
   );
 };
 export default SignUp;
