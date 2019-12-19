dd02015fa4d778019f20cb6957ab64f82f671190
Alaa Sadek <alaa.sa.mahmoud@gmail.com>
2019-12-19 20:20:08 +0200

    added admin UI

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import SignIn from './modules/signin/SignIn';
 import SignUp from './modules/signup/SignUp';
 import NavBar from './modules/navbar/NavBar';
-
+import Home from './modules/home/Home';
+import EditProduct from './modules/admin/editproduct/EditProduct';
 const App = () =>  
 {
   return (
       <div>
-        <NavBar/>
+        <EditProduct/>
+   
       </div>
   );
 }
